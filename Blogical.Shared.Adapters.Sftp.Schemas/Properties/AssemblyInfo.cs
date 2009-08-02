651ecaa0d1ab8253e643feb1adfdd65b12f2ae3b
wmmihaa_cp <SND\wmmihaa_cp>
2009-08-02 20:18:10 +0000

    Updated version numbers etc

--- Blogical.Shared.Adapters.Sftp.Schemas/Properties/AssemblyInfo.cs
+++ Blogical.Shared.Adapters.Sftp.Schemas/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("")]
+[assembly: AssemblyTitle("Blogical.Shared.Adapters.Sftp.Management")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Blogical")]
+[assembly: AssemblyProduct("Blogical.Shared.Adapters.Sftp.Management")]
+[assembly: AssemblyCopyright("Copyright © Blogical 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyDefaultAlias("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
